f05d75c70a9a91f273669197dca2628fd38e7aff
Saptarshi Chatterjee <diesel@falcon.local>
2018-12-18 16:36:27 -0600

    Works

---
+++ src/main/java/Williams/Sonoma/interview/RouteController.java
@@ -0,0 +1,16 @@
+package Williams.Sonoma.interview;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RouteController {
+    @RequestMapping(value = "/api/products/all")
+    public String getProducts() {
+        return new RestTemplate().getForObject(
+            "https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json", String.class
+        );
+    }
+}
